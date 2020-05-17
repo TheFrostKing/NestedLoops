41b6370defc0e859bf7361ef3c0b7c90435d6542
TheFrostKing <37861327+TheFrostKing@users.noreply.github.com>
2020-05-17 10:32:50 +0300

    Equal sums of odd and even numbers

---
+++ EqualSumsAnd/EqualSumsAnd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualSumsAnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++)
+            {
+               string currentNumber= i.ToString();
+                int oddCounter = 0;
+                int evenCounter = 0;
+                for (int j = 0; j < currentNumber.Length; j++)
+                {   
+                    if (j % 2 == 0)
+                    {
+                        evenCounter += int.Parse(currentNumber[j].ToString());
+                    }
+                    else
+                    {
+                        oddCounter += int.Parse(currentNumber[j].ToString());
+                    }
+
+                }
+
+                if (evenCounter==oddCounter)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
