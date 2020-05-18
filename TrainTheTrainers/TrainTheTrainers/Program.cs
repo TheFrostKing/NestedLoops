262edad4b257adf83d33f8de77eecb4a133572ac
TheFrostKing <37861327+TheFrostKing@users.noreply.github.com>
2020-05-18 08:41:58 +0300

    While loops again forgot that I can make while(true) and 1000 stupid counters

---
+++ TrainTheTrainers/TrainTheTrainers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfComittee = int.Parse(Console.ReadLine());
+            string presentation = "";
+            decimal presentationSingleScore = 0;
+            decimal counter = 0;
+            decimal scoreCounter = 0;
+            decimal allPresentationScore = 0;
+            while (true)
+            {
+                presentation = Console.ReadLine();
+                if (presentation=="Finish")
+                {
+                    decimal scoreSum = allPresentationScore / scoreCounter;
+                    Console.WriteLine($"Student's final assessment is {scoreSum:F2}.");
+                    break;
+                }
+                for (int i = 1; i <= numberOfComittee; i++)
+                {
+                    decimal grade = decimal.Parse(Console.ReadLine());
+                    counter += grade;
+                    allPresentationScore += grade;
+                    scoreCounter++;
+                    
+                }
+
+                 presentationSingleScore = counter / numberOfComittee;
+                Console.WriteLine($"{presentation} - {presentationSingleScore:F2}.");
+
+            }
+          
+
+        }
+    }
+}
