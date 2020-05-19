31bcaf0340b648ed59d38ac4b57f580f14e67a36
TheFrostKing <37861327+TheFrostKing@users.noreply.github.com>
2020-05-19 11:11:32 +0300

    Buildings

---
+++ Training/Training/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+            for (int i =floors; i>=1; i--)
+            {
+                for (int r = 0; r < rooms; r++)
+                {
+                    
+                    if (i%2==0&&floors!=i)
+                    {
+                        Console.Write($"O{i}{r} "); 
+                    }
+         
+                   if(i == floors)
+                    {
+                        Console.Write($"L{i}{r} ");
+                    }
+                    else if(i%2!=0)
+                    {
+                        Console.Write($"A{i}{r} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
