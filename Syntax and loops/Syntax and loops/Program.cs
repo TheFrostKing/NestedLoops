55134979dc8e14080c3e0f49f92cbbbfca8bc254
TheFrostKing <37861327+TheFrostKing@users.noreply.github.com>
2020-05-20 17:14:30 +0300

    triangle using nested loop

---
+++ Syntax and loops/Syntax and loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntax_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 1; i <= row; i++)
+            {
+                counter++;
+                for (int r = 1; r <= i; r++)
+                {
+                    
+                    Console.Write(counter);
+                }    
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
